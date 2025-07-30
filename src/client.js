447d237ab304017ffc3494d194e0c63d33a4939f
Khanh <khanh@starchdev.com>
2025-07-30 09:10:23 -0500

    fix: Create new database and remove API key from being pushed to repo
    Signed-off-by: Khanh <khanh@starchdev.com>

--- src/client.js
+++ src/client.js
@@ -1,7 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const URL = "https://jgfvimkbtgdbccdfgbzw.supabase.co";
-const API_KEY =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpnZnZpbWtidGdkYmNjZGZnYnp3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM4NDM4ODEsImV4cCI6MjA2OTQxOTg4MX0.IILV8vpWacCC0W_vv4yDuydnIqD9A8T6sQDkcglCe1g";
+const URL = import.meta.env.VITE_SUPABASE_URL;
+const API_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(URL, API_KEY);
--- src/pages/ReadPosts.jsx
+++ src/pages/ReadPosts.jsx
@@ -1,32 +1,33 @@
-import { useState, useEffect } from 'react'
-import Card from '../components/Card'
+import { useState, useEffect } from "react";
+import { supabase } from "../client";
+import Card from "../components/Card";
 
 const ReadPosts = (props) => {
-
-    const [posts, setPosts] = useState([])
+    const [posts, setPosts] = useState([]);
 
     useEffect(() => {
-        setPosts(props.data)
-    }, [props])
-    
+        setPosts(props.data);
+    }, [props]);
+
     return (
         <div className="ReadPosts">
-            {
-                posts && posts.length > 0 ?
+            {posts && posts.length > 0 ? (
                 [...posts]
-                .sort((a, b) => a.id - b.id)
-                .map((post,index) => 
-                    <Card 
-                        key={post.id}
-                        id={post.id} 
-                        title={post.title}
-                        author={post.author}
-                        description={post.description}
-                    />
-                ) : <h2>{'No Challenges Yet 😞'}</h2>
-            }
-        </div>  
-    )
-}
+                    .sort((a, b) => a.id - b.id)
+                    .map((post, index) => (
+                        <Card
+                            key={post.id}
+                            id={post.id}
+                            title={post.title}
+                            author={post.author}
+                            description={post.description}
+                        />
+                    ))
+            ) : (
+                <h2>{"No Challenges Yet 😞"}</h2>
+            )}
+        </div>
+    );
+};
 
-export default ReadPosts
+export default ReadPosts;
